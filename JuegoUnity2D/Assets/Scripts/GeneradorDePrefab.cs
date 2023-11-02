d540828ca62b0743655f207ae3959a7d9ef42eea
Ivaneitor47 <112414614+RowsMush022@users.noreply.github.com>
2023-11-02 11:13:25 -0300

    Invoke.Inoke repeating y courutine
    Se aplico estos 3 métodos especiales al generador de enemigos 1

--- JuegoUnity2D/Assets/Scripts/GeneradorDePrefab.cs
+++ JuegoUnity2D/Assets/Scripts/GeneradorDePrefab.cs
@@ -7,25 +7,39 @@ public class GeneradorDePrefab : MonoBehaviour
     public Transform posicionGeneracion;
     public float tiempoMinimo = 1.0f;
     public float tiempoMaximo = 3.0f;
+    public float intervaloInvokeRepeating = 5.0f;
 
     void Start()
     {
-        
+        // Usar InvokeRepeating para generar prefabs cada 'intervaloInvokeRepeating' segundos.
+        InvokeRepeating("GenerarPrefabConInvokeRepeating", 0f, intervaloInvokeRepeating);
+
+        // Usar una Coroutine para generar prefabs con intervalos aleatorios.
         StartCoroutine(GenerarPrefabPeriodicamente());
+    }
+
+    void GenerarPrefabConInvokeRepeating()
+    {
+        // Generar un prefab en la posición de generación.
+        Instantiate(prefabAGenerar, posicionGeneracion.position, Quaternion.identity);
     }
 
     IEnumerator GenerarPrefabPeriodicamente()
     {
         while (true)
         {
-            
             float tiempoEspera = Random.Range(tiempoMinimo, tiempoMaximo);
 
-            
             yield return new WaitForSeconds(tiempoEspera);
 
-            
-            Instantiate(prefabAGenerar, posicionGeneracion.position, Quaternion.identity);
+            // Generar un prefab utilizando Invoke para un único evento en un tiempo aleatorio.
+            Invoke("GenerarPrefabConInvoke", 0f);
         }
+    }
+
+    void GenerarPrefabConInvoke()
+    {
+        // Generar un prefab en la posición de generación.
+        Instantiate(prefabAGenerar, posicionGeneracion.position, Quaternion.identity);
     }
 }
